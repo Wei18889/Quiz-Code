a0bfcc6e5acd91a7d080f5817b2ed44364d257de
Weiyu Gao <weiyugao@weiyu.local>
2023-07-27 18:31:36 -0700

    Quiz

---
+++ README.md
@@ -0,0 +1,24 @@
+# Code-Quiz!
+
+Welcome to Code-Quiz! This is a simple yet engaging application that features three coding-related questions. No matter where you are on your coding journey, it's always rewarding to track your progress by answering questions that might have seemed challenging before.
+
+## Usage
+
+Getting started with Code-Quiz is easy. Begin by entering your initials or a nickname. This will be saved locally so you can keep track of your high scores. Once you've entered your name, you'll be presented with three questions. After answering all the questions, hit the 'Submit' button to see your score.
+
+## Deployed Link
+
+
+
+## Preview
+
+![Quiz Preview 1](image/Screenshot_2023-07-27_at_6.21.25_PM.png)
+![Quiz Preview 2](image/Screenshot_2023-07-27_at_6.21.45_PM.png)
+
+## Customization
+
+Feel free to customize the quiz to suit your needs. You can modify the quiz array in the JavaScript code to add your own questions, choices, and answers. If you want to adjust the time limit for the quiz, simply change the `quizTime` variable.
+
+## Note
+
+This application uses local storage to save high scores. If your browser does not support local storage, the high scores feature will not work.
---
+++ script.js
@@ -0,0 +1,86 @@
+// Quiz Questions
+const quiz = [
+    {
+      question: "Which command will generate a new Angular component?",
+      choices: ["A. ng g component", "B. ng generate component", "C. ng new component", "D. Both A and B"],
+      answer: "D. Both A and B"
+    },
+    {
+      question: "Which library is used for state management in React?",
+      choices: ["A. Redux", "B. React-Redux", "C. React-router-dom", "D. Both A and B"],
+      answer: "D. Both A and B"
+    },
+    {
+      question: "What is Node.js?",
+      choices: ["A. A server-side platform", "B. A JavaScript runtime built on Chrome's V8 JavaScript engine", "C. An open source server environment.", "D. All of the above"],
+      answer: "D. All of the above"
+    }
+  ];
+  
+  // Quiz Timer in seconds
+  const quizTime = 30;
+  let timer = null;
+  
+  // High Scores Array
+  let highScores = [];
+  
+  // Start The Quiz
+  function startQuiz() {
+    // Hides The Start Button and Initials Input
+    document.querySelector("button").style.display = "none";
+    document.querySelector("input").style.display = "none";
+  
+    // Displays Quiz Questions and Choices
+    let quizHtml = "";
+    for (let i = 0; i < quiz.length; i++) {
+      quizHtml += "<p>" + quiz[i].question + "</p>";
+      for (let j = 0; j < quiz[i].choices.length; j++) {
+        quizHtml += "<input type='radio' name='q" + i + "' value='" + quiz[i].choices[j] + "'>" + quiz[i].choices[j] + "<br>";
+      }
+    }
+    document.querySelector("#quiz").innerHTML = quizHtml;
+  
+    // Show the submit button
+    document.querySelector("#submitQuiz").style.display = "";
+  
+    // Start Quiz Timer
+    let timeLeft = quizTime;
+    timer = setInterval(function() {
+      document.querySelector("#score").innerHTML = "Time left: " + timeLeft;
+      timeLeft--;
+      if (timeLeft < 0) {
+        clearInterval(timer);
+        submitQuiz();
+      }
+    }, 1000);
+  }
+  
+  // Submits The Quiz and Display The Score
+  function submitQuiz() {
+    // Get's The User's Initials
+    let initials = document.querySelector("#initials").value;
+    let score = 0;
+    for (let i = 0; i < quiz.length; i++) {
+      let selected = document.querySelector("input[name='q" + i + "']:checked");
+      if (selected && selected.value === quiz[i].answer) {
+        score++;
+      }
+    }
+  
+    // Saves The User's Score Locally 
+    highScores.push({initials: initials, score: score});
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+  
+    // Display The Score
+    document.querySelector("#score").innerHTML = "Your score is " + score + " out of " + quiz.length;
+  
+    // Clear quiz and the timer without hiding the user's score
+    document.querySelector("#quiz").innerHTML = '';
+    clearInterval(timer);
+  }
+  
+  // Loads The High Scores From Local Storage
+  if (localStorage.getItem("highScores")) {
+    highScores = JSON.parse(localStorage.getItem("highScores"));
+  }
+  
